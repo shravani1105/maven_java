adb1d05e362b821c1e49fbe720e4dd33a8e56d60
Shravani <uppalashravani205@gmail.com>
2024-11-19 11:04:31 +0530

    maven_java

---
+++ src/main/java/MAVEN_JAVA/T/App.java
@@ -0,0 +1,13 @@
+package MAVEN_JAVA.T;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a=9;
+        int b=10;
+        int c=a+b;
+        System.out.println("The sum of the numbers is: "+c);
+    }
+}
